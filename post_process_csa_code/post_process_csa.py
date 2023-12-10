b119257ce904162ed50e1e6645aa64ab00f6ef5f
Alex Partin <25892264+adpartin@users.noreply.github.com>
2023-12-09 21:19:27 -0800

    start updating csa postprocessing

--- post_process_csa_code/post_process_csa.py
+++ post_process_csa_code/post_process_csa.py
@@ -8,21 +8,33 @@ import json
 from sklearn.metrics import r2_score, mean_absolute_error
 from scipy.stats import pearsonr, spearmanr, sem
 
+from improve.metrics import compute_metrics
 
 fdir = Path(__file__).resolve().parent
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_name', required=False, default='LGBM', type=str,
                     help='Name of the model.')
+parser.add_argument('--y_col_name', required=False, default='auc', type=str,
+                    help='Y col name.')
 args = parser.parse_args()
-model_name = args.model_name
+
+y_col_name = args.y_col_name
+
+main_dir_path = Path("/lambda_stor/data/apartin/projects/IMPROVE/pan-models/GraphDRP")
+infer_dir_name = "infer"
+infer_dir_path = main_dir_path/y_col_name/infer_dir_name
+dirs = list(infer_dir_path.glob("*-*")); print(dirs)
+# print(split_files)
+
+# model_name = args.model_name
 # model_name = "GraphDRP_01"
 # model_name = "GraphDRP_02"
 # model_name = "GraphDRP_03"
 
-datadir = fdir/f"results.{model_name}"
-outdir = fdir/f"scores.{model_name}"
-os.makedirs(outdir, exist_ok=True)
+# datadir = fdir/f"results.{model_name}"
+# outdir = fdir/f"scores.{model_name}"
+# os.makedirs(outdir, exist_ok=True)
 
 
 data_sources = ["ccle", "ctrp", "gcsi", "gdsc1", "gdsc2"]
@@ -46,40 +58,58 @@ scores_names = {"mae": calc_mae,
                 "pcc": calc_pcc,
                 "scc": calc_scc}
 
-# import ipdb; ipdb.set_trace(context=5)
-# # Data source study
-# for src in data_sources:
-#     print("Source study:", src)
-#     resdir = fdir/f"results.csa.{src}"
-
-#     # Data test study
-#     # import ipdb; ipdb.set_trace(context=5)
-#     for trg in data_sources:
-#         print("Traget study:", trg)
-#         if trg not in scores:
-#             scores[trg] = {sc: [] for sc in scores_names}
-
-#         # Data split
-#         # import ipdb; ipdb.set_trace(context=5)
-#         for split in range(9):
-#             fname = f"{src}_{trg}_split_{split}.csv"
-#             df = pd.read_csv(resdir/fname)
-
-#             for sc_name, sc_func in scores_names.items():
-#                 # sc_value = sc_func(y_true=df["True"].values, y_pred=df["Pred"].values)
-#                 y_true = df["True"].values
-#                 y_pred = df["Pred"].values
-#                 sc_value = sc_func(y_true=y_true, y_pred=y_pred)
-#                 scores[trg][sc_name].append(sc_value)
-
-#     # import ipdb; ipdb.set_trace(context=5)
-#     sc_dict[src] = scores
-
-
 # ====================
 # Aggregate raw scores
 # ====================
 
+scores_file_name = "test_scores.json"
+preds_file_name = "test_y_data_predicted.csv"
+metrics_list = ["mse", "rmse", "pcc", "scc", "r2"]  
+
+dct = {}
+
+for dir_path in dirs:
+    print("\nExperiment:", dir_path)
+    # import ipdb; ipdb. set_trace()
+    src = str(dir_path.name).split("-")[0]
+    trg = str(dir_path.name).split("-")[1]
+    split_dirs = list((dir_path).glob(f"split_*"))
+
+    import ipdb; ipdb. set_trace()
+    for split_dir in split_dirs:
+        # # Load scores
+        # scores_file_path = split_dir/scores_file_name
+        # with open(scores_file_path) as json_file:
+        #     scores = json.load(json_file)
+
+        split = int(split_dir.name.split("split_")[1])
+
+        # Load predictions
+        preds_file_path = split_dir/preds_file_name
+        df = pd.read_csv(preds_file_path, sep=",")
+        # df = pd.read_csv(resdir/fname, sep="\t")
+
+        # for sc_name, sc_func in scores_names.items():
+        y_true = df[f"{y_col_name}_true"].values
+        y_pred = df[f"{y_col_name}_pred"].values
+        # ---
+        scores = compute_metrics(y_true, y_pred, metrics_list)
+
+        dct[split] = scores
+        df = pd.DataFrame(dct)
+        df = df.T.reset_index().rename(columns={"index": "split"})
+
+        # TODO. continue
+
+        # sc_value = sc_func(y_true=y_true, y_pred=y_pred)
+        # # scores[trg][sc_name].append(sc_value)
+        # import ipdb; ipdb. set_trace()
+        # scores[trg].append(sc_value)
+        
+    import ipdb; ipdb. set_trace()
+    kk = pd.DataFrame(dct)
+
+# ---------------------
 # Data source study
 for sc_name, sc_func in scores_names.items():
     print("\nMetric:", sc_name)
@@ -113,6 +143,9 @@ for sc_name, sc_func in scores_names.items():
         with open(outdir/f"{sc_name}_{src}_scores_raw.json", "w") as json_file:
             json.dump(scores, json_file)
 del scores
+# ---------------------
+
+
 
 # ====================
 # Generate tables
